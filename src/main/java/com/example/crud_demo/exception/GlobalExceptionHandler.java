cd8378a4fc11d3f29aafc3249a5edb4947176943
Marcus Chan <marcuschanjh@gmail.com>
2024-09-04 19:50:13 +0800

    Remove warnings from Code Analysis

--- src/main/java/com/example/crud_demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/crud_demo/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.example.crud_demo.exception;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
