dbe925e475a342b29c3a70f877781116bcd57f40
Marcus Chan <marcuschanjh@gmail.com>
2024-09-06 18:49:08 +0800

    Update stuff

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 sudo yum install java-17-amazon-corretto-devel git tmux wget -y
 CLOUDWATCH_AGENT_DOWNLOAD_URL="https://amazoncloudwatch-agent.s3.amazonaws.com/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm"
-JAVA_INSTRUMENTATION_AGENT_DOWNLOAD_URL="https://github.com/aws-observability/aws-otel-java-instrumentation/releases/latest/download/aws-opentelemetry-agent.jar"
 wget $CLOUDWATCH_AGENT_DOWNLOAD_URL
 sudo rpm -U ./amazon-cloudwatch-agent.rpm
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:cloudwatch-agent.json
+export AWS_ADOT_JAVA_INSTRUMENTATION_PATH=./aws-opentelemetry-agent.jar
+JAVA_TOOL_OPTIONS=" -javaagent:$AWS_ADOT_JAVA_INSTRUMENTATION_PATH" \
+OTEL_METRICS_EXPORTER=none \
+OTEL_LOGS_EXPORT=none \
+OTEL_AWS_APPLICATION_SIGNALS_ENABLED=true \
+OTEL_AWS_APPLICATION_SIGNALS_EXPORTER_ENDPOINT=http://localhost:4316/v1/metrics \
+OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf \
+OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://localhost:4316/v1/traces \
+OTEL_RESOURCE_ATTRIBUTES="service.name=$YOUR_SVC_NAME" \
+java -jar $MY_JAVA_APP.jar
