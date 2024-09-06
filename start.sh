230269a55d248c3aca2c24c91be6d11d63dcd1b5
Marcus Chan <marcuschanjh@gmail.com>
2024-09-06 19:01:19 +0800

    Update stuff

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 sudo yum install java-17-amazon-corretto-devel git tmux wget -y
+sudo yum install maven -y
 CLOUDWATCH_AGENT_DOWNLOAD_URL="https://amazoncloudwatch-agent.s3.amazonaws.com/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm"
 wget $CLOUDWATCH_AGENT_DOWNLOAD_URL
 sudo rpm -U ./amazon-cloudwatch-agent.rpm
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:cloudwatch-agent.json
+
+
+mvn compile
+mvn package
+
 export AWS_ADOT_JAVA_INSTRUMENTATION_PATH=./aws-opentelemetry-agent.jar
 JAVA_TOOL_OPTIONS=" -javaagent:$AWS_ADOT_JAVA_INSTRUMENTATION_PATH" \
 OTEL_METRICS_EXPORTER=none \
